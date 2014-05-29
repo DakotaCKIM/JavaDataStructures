555c2cdbcbd558ed733304b60028cd5245700a6b
DakotaCKim <Dakota.Kim001@umb.edu>
2014-05-29 15:58:16 -0400

    Update

--- LineUsageData.java
+++ LineUsageData.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class LineUsageData {
 
 	// Since we are going to hold two values, username and count.
-	// I am going to implement a Map<String, Integer>.
+	// I am going to implement a SinglyLinkedList holding Usage objects.
 
 	private SinglyLinkedList<Usage> usageList;
 
@@ -15,21 +15,7 @@ public class LineUsageData {
 		usageList = new SinglyLinkedList<Usage>();
 	}
 
-	// add one sighting of a use on this line
-	public void addObservation(String username) {
-		Usage usage;
-		for (int i = 0; i < usageList.size(); i++) {
-			usage = usageList.get(i);
-			// If the user has never been seen before, it creates a new usage for that user.
-			if (usage == null)
-				usage = new Usage(username);
-			usage.increment();
-			usageList.add(usage);
-		}
-
-	}
-
-	// find the user with the most sightings on this line
+	// Find the user with the most sightings on this line
 	public Usage findMaxUsage() {
 		Usage user;
 		Usage highScore = new Usage("<NONE>");  // with count 0
@@ -40,6 +26,19 @@ public class LineUsageData {
 				highScore = user;
 		}
 		return highScore;
+	}
+
+	// Add one sighting of a use on this line
+	public void addObservation(String username) {
+		Usage usage;
+		for (int i = 0; i < usageList.size(); i++) {
+			usage = usageList.get(i);
+			// If the user has never been seen before, it creates a new usage for that user.
+			if (usage == null)
+				usage = new Usage(username);
+			usage.increment();
+			usageList.add(usage);
+		}
 	}
 }
 
